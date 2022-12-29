95c23a83b09321d60359fae5ca39fbe50152d159
Patrick Fu <paaatrickfu@gmail.com>
2022-12-30 00:47:18 +0800

    ✨ Init swift package

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-audio-ios
+
+A description of this package.
---
+++ Sources/express-audio-ios/express_audio_ios.swift
@@ -0,0 +1,6 @@
+public struct express_audio_ios {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
