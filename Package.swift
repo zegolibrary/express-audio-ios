bef785646c7727f26c570c9951c6fab20ceb30fa
zegolibrary <developer@zego.im>
2024-04-23 21:33:27 +0800

    🔖 Release 3.14.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZegoExpressEngine",
-            url: "https://artifact-node.zego.cloud/generic/swiftpm/public/express-audio-ios/ZegoExpressEngine.xcframework.zip?version=3.13.2",
-            checksum: "c9468e24673d17d6d1183fafc129d8e0756ac731a50b2ce497e9198a875b664b"
+            url: "https://artifact-node.zego.cloud/generic/swiftpm/public/express-audio-ios/ZegoExpressEngine.xcframework.zip?version=3.14.0",
+            checksum: "bb01e49eaf42ec3ee6c65402e9158f3858ee728b2dc744da1ee582eddf98f516"
         )
     ]
 )
